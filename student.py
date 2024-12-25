c59ddd8e71ef69d071d67e77cd15914319766dc7
Şeyma Ekici <seymaekici45@gmail.com>
2024-12-25 13:29:00 +0300

    First commit

---
+++ student.py
@@ -0,0 +1,7 @@
+students = [
+    {"name": "Şeyma", "grade": 99},
+    {"name": "Mert", "grade": 98}
+]
+
+for student in students:
+    print(f"Student: {student['name']}, Grade: {student['grade']}")
